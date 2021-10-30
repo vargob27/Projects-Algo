59c322bea8bd5b9f246671e4e053286d363dd3ff
Bryce <80635028+vargob27@users.noreply.github.com>
2021-10-29 22:05:30 -0400

    Update arraysToDo2.js

--- arraysToDo2.js
+++ arraysToDo2.js
@@ -27,5 +27,3 @@ reverseArr(test);
 // Concat
 // Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
 // array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
-
-a;sldkfja;lsdkfj
