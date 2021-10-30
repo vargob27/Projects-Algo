240861c8e07b3422371c4e207dd9e699876daa71
Bryce <80635028+vargob27@users.noreply.github.com>
2021-10-29 22:03:10 -0400

    Arrays 2 assignment

---
+++ arraysToDo2.js
@@ -0,0 +1,29 @@
+// Reverse
+// Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
+// array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
+
+function reverseArr(arr) {
+    for (i = 0; i < arr.length / 2; i++) {
+        let t = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = t;
+    }
+    console.log(arr);
+    return arr;
+}
+var test = [1,3,5,7];
+reverseArr(test);
+
+// Rotate
+// Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
+// array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
+
+
+// Filter Range
+// Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
+// array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
+
+
+// Concat
+// Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
+// array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
