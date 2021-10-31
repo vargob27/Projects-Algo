587b6da7d6bbbd39c5b27b38ab1bd85819a636cb
Bryce <80635028+vargob27@users.noreply.github.com>
2021-10-31 11:05:31 -0400

    Project update / array assignment progress
    Worked on my solo project a bit and arrays assignment

--- arraysToDo1.js
+++ arraysToDo1.js
@@ -1,6 +1,5 @@
 // Push Front
 // Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.
-
 function pushFront(arr, num) {
     let outputArr = [num, ...arr];
     return outputArr;
--- arraysToDo2.js
+++ arraysToDo2.js
@@ -4,24 +4,75 @@
 
 function reverseArr(arr) {
     for (i = 0; i < arr.length / 2; i++) {
-        let t = arr[i];
+        let temp = arr[i];
         arr[i] = arr[arr.length - 1 - i];
-        arr[arr.length - 1 - i] = t;
+        arr[arr.length - 1 - i] = temp;
     }
     console.log(arr);
     return arr;
 }
-var test = [1,3,5,7];
-reverseArr(test);
+var test = [1,3,5,7,9,11];
+// reverseArr(test);
 
 // Rotate
-// Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
-// array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
-
+// Implement rotateArr(arr, shiftBy) that accepts array and offset. Shift arr's values to the right by that amount. 'Wrap-around' any values that shift off array's end to the other side, so that no data is lost. Operate in-place: given ([1,2,3],1), 
+// change the array to [3,1,2]. Don't use built-in functions.
+// Second: allow negative shiftBy (shift L, not R).
+// Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
+// Fourth: minimize the touches of each element.
+function rotate(arr, offset) {
+    // if (offset > 0) {
+    //     for (i = 0; i < offset; i++) {
+    //         let temp = arr[arr.length -1];
+    //         for (i2 = arr.length - 2; i2 >= 0; i2--) {
+    //             arr[i2 + 1] = arr[i2];
+    //         }
+    //         arr[0] = temp;
+    //         console.log(arr);
+    //     }
+    //     return arr;
+    // } else {
+    //     let convert = offset + offset*2
+    //     for (i = 0; i < convert; i--) {
+    //         let temp = arr[0];
+    //         for (i2 = 1; i2 < arr.length; i2++) {
+    //             arr[i2-1] = arr[i2];
+    //         }
+    //         arr[arr.length-1] = temp;
+    //         console.log(arr);
+    //     }
+    //     return arr;
+    // }
+    var actual;
+    if (offset > 0) {
+        actual = offset % arr.length;
+    } else {
+        actual = Math.abs(offset) % arr.length;
+    }
+    if (offset > 0) {
+        for (var i = 0; i < actual; i++) {
+            var temp = arr[arr.length - 1];
+            for (var i2 = arr.length - 2; i2 >= 0; i2--) {
+                arr[i2+1] = arr[i2];
+            }
+            arr[0] = temp;
+        }
+    } else {
+        for (var i = 0; i < actual; i++) {
+            var temp = arr[0];
+            for (var i2 = 1; i2 < arr.length; i2++) {
+                arr[i2 - 1] = arr[i2];
+            }
+            arr[arr.length - 1] = temp;
+        }
+    }
+    return arr;
+}
+var test2 = [1,3,5,7];
+console.log(rotate(test2, -1));
 
 // Filter Range
-// Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
-// array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
+// Alan is good at breaking secret codes. One method is to eliminate values that lie outside of a specific known range. Given arr and values min and max, retain only the array values between min and max. Work in-place: return the array you are given, with values in original order. No built-in array functions.
 
 
 // Concat
