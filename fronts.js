e58226d860f8b3e1e087ef9e3546c542163dec9c
Bryce <80635028+vargob27@users.noreply.github.com>
2021-11-02 21:44:22 -0400

    fronts

--- fronts.js
+++ fronts.js
@@ -1,6 +1,43 @@
+class Node {
+    constructor(value) {
+        this.val = value;
+        this.next = null;
+    }
+}
+
 class SinglyLinkedList {
     constructor() {
         this.head = null;
     }
 
+    addToFront(val) {
+        var newNode = new Node(val);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    removeFront() {
+        if (this.head == null) {
+            return this.head;
+        }
+        let removed = this.head;
+        this.head = removed.next;
+        removed.next = null;
+        return this.head;
+    }
+
+    front() {
+        if (this.head == null) {
+            return this.head;
+        }
+        return this.head.val;
+    }
 }
+
+var mySLL = new SinglyLinkedList();
+mySLL.addToFront(5);
+mySLL.addToFront(69);
+console.log(mySLL);
+console.log(mySLL.removeFront());
+console.log(mySLL);
+console.log(mySLL.front());
